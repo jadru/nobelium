63e25ca83ccc039ddcf220c762ccc2118dee3a4a
Younggun Park <ygpark0922@gmail.com>
2022-06-13 22:59:18 +0900

    feat: change error

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Younggun',
   email: 'ygpark0922@gmail.com',
   link: 'https://github.com/jadru',
-  description: 'Hello world! it's frontend developer here:),
+  description: 'Hello world:)',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
